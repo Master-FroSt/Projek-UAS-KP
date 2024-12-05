1691d79b34da2d43c879955bb5cb7072a828cd7b
Master-Frost <mfschannel30@gmail.com>
2024-12-05 13:53:45 +0700

    Delete owner Mode

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#define SIZE 10
+#define SIZE 64
 #define PAJAK 0.1
 
-/*
-mode owner, dapat memastikan stok dan mengubah harga */
-
 void printMenu(int harga[]);
-void pesanan(int count, int pesan, int pilihan[], int stok[]);
+void pesanan(int count, int pesan, int pilihan[]);
 void printHarga(int count, int pilihan[], int harga[], double hargaTotal);
-void ubahArray(int index, int nilai, int array[]);
 
 int main(){
     //deklarasi dan inisialisasi
-    int stok[SIZE], pilihan[SIZE];
+    int pilihan[SIZE];
     int harga[SIZE] = {
         15000, // Paket Hemat
         17000, // Paket Spesial
@@ -39,36 +35,13 @@ int main(){
     };
     double hargaTotal = 0.0;
 
-    //Pemilihan mode beli dan Admin
-    int mode;
-    puts("Pilih Mode Program:\n1. Beli\n2. Admin");
-    while(1){
-        scanf("%d", &mode);
-        switch(mode){
-            case 1:
-            puts("Silakan pilih makanan yang ingin dipesan");
-            break;
-            case 2:
-            puts("1. Manajemen Stok\n2.Manajemen Harga");
-            break;
-            default:
-            puts("Masukan salah, silakan isi kembali!");
-        }
-    if(mode == 1 || mode == 2) break;
-    }
-    
-    //Mode User
     int count = 0;
+    printMenu(harga);
     while(1){
         int pesan;
-        printMenu(harga);
         printf("Pilih menu yang diinginkan (-1 untuk melakukan pembayaran): ");
         scanf("%d", &pesan);
-        if(stok == 0) {
-            printf("Maaf stok telah habis. Silakan pilih menu lain!");
-        } else {
-            pesanan(count, pesan, pilihan, stok);
-        }
+        pesanan(count, pesan, pilihan);
         count++;
         if(pesan == -1) break;
     }
@@ -77,8 +50,8 @@ int main(){
     int metodePembayaran;
     printHarga(count, pilihan, harga, hargaTotal);
     while(1){
-        scanf("%d", &mode);
-        switch(mode){
+        scanf("%d", &metodePembayaran);
+        switch(metodePembayaran){
             case 1:
             puts("Metode Cash dipilih.");
             break;
@@ -88,20 +61,28 @@ int main(){
             default:
             puts("Masukan salah, silakan isi kembali!");
         }
-    if(mode == 1 || mode == 2) break;
+    if(metodePembayaran == 1 || metodePembayaran == 2) break;
     }
     printf("Terima kasih telah bertransaksi!");
     printf("Silakan ditunggu masakannya!");
-
-    //Mode Owner
 
     return 0;
 }
 
 void printMenu(int harga[]) {
+    for (int i = 0; i < 44; i++) {
+        printf("=");
+    } 
+    printf(" Menu Paket ");
+    for (int i = 0; i < 43; i++) {
+        printf("=");
+    }
+    printf("\n");
+
     printf("1. Paket Hemat    (Ayam Goreng, Nasi Putih, Sambal, Es Teh/Es Jeruk)                   = Rp. %d\n", harga[0]);
     printf("2. Paket Spesial  (Ayam Goreng, Nasi Putih, Sambal, Telur, Es Teh/Es Jeruk)            = Rp. %d\n", harga[1]);
     printf("3. Paket Komplit  (Ayam Goreng, Nasi Putih, Sambal, Telur, Sayur, Es Teh/Es Jeruk)     = Rp. %d\n", harga[2]);
+
     printf("\n");
 
     for (int i = 0; i < 44; i++) {
@@ -126,6 +107,7 @@ void printMenu(int harga[]) {
     printf("14. Ayam Bakar                                                                         = Rp. %d\n", harga[13]);
     printf("15. Ayam Teriyaki                                                                      = Rp. %d\n", harga[14]);
 
+    printf("\n");
     for (int i = 0; i < 45; i++) {
         printf("=");
     } 
@@ -142,21 +124,18 @@ void printMenu(int harga[]) {
     printf("20. Es Cincau                                                                          = Rp. %d\n", harga[19]);
     printf("21. Es Kelapa Muda                                                                     = Rp. %d\n", harga[20]);
     printf("22. Es Cappucino                                                                       = Rp. %d\n", harga[21]);
-
-    return 0;
 }
 
 
-void pesanan(int count, int pesan, int pilihan[], int stok[]) {
-    stok[pesan] -= 1;
+void pesanan(int count, int pesan, int pilihan[]) {
     pilihan[count] = pesan;
 }
 
 void printHarga(int count, int pilihan[], int harga[], double hargaTotal) {
     for(int i = 0; i < count; i++){
-        hargaTotal += harga[pilihan[i]];
+        hargaTotal += harga[pilihan[i] - 1];
     }
-    hargaTotal *= PAJAK;
-    printf("Rp%-13.2lf", hargaTotal);
+    hargaTotal += (hargaTotal * PAJAK);
+    printf("Rp%.2lf\n", hargaTotal);
     puts("Pilih metode pembayaran:\n1. Cash\n2. Cashless");
 }
